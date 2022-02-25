d0ef3a567cf46f733ba831248412fd66536e13bd
fahim100 <fahimahmmed100@gmail.com>
2022-02-25 20:18:34 +0600

    Added some css.

--- order-details.php
+++ order-details.php
@@ -9,36 +9,34 @@ function order_details_shortcode(){
 
     ?>
     <section class="order-details">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="order-info">
-                        <div class="order-id items">
-                            <div class="content">
-                                <h5>Order ID</h5>
-                                <span>#<?php echo $order_id; ?></span>
-                            </div>
-                            <div class="icon">
-                                <i class="ri-information-fill"></i>
-                            </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="order-info">
+                    <div class="order-id items">
+                        <div class="content">
+                            <h5>Order ID</h5>
+                            <span>#<?php echo $order_id; ?></span>
                         </div>
-                        <div class="date-formation items">
-                            <div class="content">
-                                <h5>Date of Formation</h5>
-                                <span><?php echo $date_of_formation; ?></span>
-                            </div>
-                            <div class="icon">
+                        <div class="icon">
+                            <i class="ri-information-fill"></i>
+                        </div>
+                    </div>
+                    <div class="date-formation items">
+                        <div class="content">
+                            <h5>Date of Formation</h5>
+                            <span><?php echo $date_of_formation; ?></span>
+                        </div>
+                        <div class="icon">
                             <i class="ri-calendar-check-line"></i>
-                            </div>
                         </div>
-                        <div class="ein-info items">
-                            <div class="content">
-                                <h5>EIN(Fedarel Tax ID)</h5>
-                                <span><?php echo $ein_no; ?></span>
-                            </div>
-                            <div class="icon">
+                    </div>
+                    <div class="ein-info items">
+                        <div class="content">
+                            <h5>EIN(Fedarel Tax ID)</h5>
+                            <span><?php echo $ein_no; ?></span>
+                        </div>
+                        <div class="icon">
                             <i class="ri-shield-check-fill"></i>
-                            </div>
                         </div>
                     </div>
                 </div>
